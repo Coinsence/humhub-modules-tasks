51635e6bfc1c3a202ea3cd21b4e98c1fad74fe5d
Coinsence.org <41925899+KarimChabrak@users.noreply.github.com>
2021-01-13 18:21:43 +0100

    Update views_notifications_invited.php

--- messages/ar/views_notifications_invited.php
+++ messages/ar/views_notifications_invited.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    '{userName} assigned you as responsible person in task {task} from space {spaceName}.' => '{spaceName} من الفضاء {task} كلفك كمسؤول في مهمة {userName}',
+    '{userName} assigned you as responsible person in task {task} from space {spaceName}.' => '{task} كلفك كمسؤول في مهمة {userName}',
 ];
